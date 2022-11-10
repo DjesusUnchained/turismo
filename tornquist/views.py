0098767e591d96f8f9945701a3f408eb1f6e3d31
Yoalfre <yoalfreparra@gmail.com>
2022-11-10 20:43:26 -0300

    se agregan templates nuevos y se agrega dinamismo en la pag gastronomia

--- tornquist/views.py
+++ tornquist/views.py
@@ -8,7 +8,53 @@ def index(request):
     return render(request,'tornquist/publica/index.html')
 
 def gastronomia(request):
-    return render(request,'tornquist/publica/gastronomia.html')
+    restaurantes = [
+        {
+            'nombre':'Molino Azul - Sierra de la Ventana',
+            'ubicacion': 'Sierra de la Ventana (Bs As - Argentina)',
+            'telefono':'(+54) 0291-4941075',
+            'direccion':'Av San Martin 114, Sierra de la Ventana Bs. As.',
+            'pagina_web':'MolinoAzul.com.ar',
+            'url_img':'/static/tornquist/publica/img/molino-azul.jpg',
+        },
+
+        {
+            'nombre':'Sol y Luna - Sierra de la Ventana',
+            'ubicacion': 'Sierra de la Ventana (Bs As - Argentina)',
+            'telefono':'(+54) 0291-4941075',
+            'direccion':'Av. San Marti 353, Sierra de la Ventana Bs. As.',
+            'pagina_web':'MolinoAzul.com.ar',
+            'url_img':'/static/tornquist/publica/img/sol-y-luna-restaurante.jpg',
+        },
+
+        {
+            'nombre':'Restaurant Urbano - Tornquist',
+            'ubicacion': 'Tornquist (Bs As - Argentina)',
+            'telefono':'(+54) 0291-4941075',
+            'direccion':'Drago 53, Tornquist Bs. As.',
+            'pagina_web':'RestaurantUrbano.com.ar',
+            'url_img':'/static/tornquist/publica/img/restaurantUrbano.jpg',
+        },
+
+        {
+            'nombre':'Hosteria La Peninsula - Villa Ventana',
+            'ubicacion': 'Villa Ventana (Bs As - Argentina)',
+            'telefono':'(+54) 0291-4941075',
+            'direccion':'Drago 53, Villa Ventana Bs. As.',
+            'pagina_web':'LaPeninsula.com.ar',
+            'url_img':'/static/tornquist/publica/img/hosteria-la-peninsula.jpg',
+        },
+
+        {
+            'nombre':'Ristorante Da Roberto - Villa Ventana',
+            'ubicacion': 'Villa Ventana (Bs As - Argentina)',
+            'telefono':'(+54) 0291-4941075',
+            'direccion':'Drago 53, Villa Ventana Bs. As.',
+            'pagina_web':'DaRoberto.com.ar',
+            'url_img':'/static/tornquist/publica/img/ristoranteDaRoberto.jpg',
+        }
+    ]
+    return render(request,'tornquist/publica/gastronomia.html',{'restaurantes':restaurantes})
 
 def zonasAlojamientos(request):
     return render(request,'tornquist/publica/zonasAlojamientos.html')
